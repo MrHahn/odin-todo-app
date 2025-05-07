061553adde16310af17beda7069b4cdb41ba2bff
Christopher Hahn <chahn970@gmail.com>
2025-05-06 20:06:02 -0600

    add date to forms, additional styling

--- src/ProjectUI.js
+++ src/ProjectUI.js
@@ -13,11 +13,16 @@ export default class ProjectUI{
             const todoTitle = document.createElement('h1');
             const todoDesc = document.createElement('p');
             const close = document.createElement('span');
+            const todoDate = document.createElement('span');
 
             todoWrapper.classList.add('todo');
             todoWrapper.setAttribute('data-index', index);
+            todoTitle.classList.add('todo-title');
             todoTitle.textContent = item.title;
+            todoDesc.classList.add('todo-description');
             todoDesc.textContent = item.description;
+            todoDate.classList.add('due-date');
+            todoDate.textContent = `Due: ${item.dueDate}`;
             close.classList.add('close');
             close.innerHTML = '<svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 24"><title>close-circle</title><path d="M12,2C17.53,2 22,6.47 22,12C22,17.53 17.53,22 12,22C6.47,22 2,17.53 2,12C2,6.47 6.47,2 12,2M15.59,7L12,10.59L8.41,7L7,8.41L10.59,12L7,15.59L8.41,17L12,13.41L15.59,17L17,15.59L13.41,12L17,8.41L15.59,7Z" /></svg>';
 
@@ -29,6 +34,7 @@ export default class ProjectUI{
 
             todoWrapper.appendChild(todoTitle);
             todoWrapper.appendChild(todoDesc);
+            todoWrapper.appendChild(todoDate);
 
             container.appendChild(todoWrapper);
         })
